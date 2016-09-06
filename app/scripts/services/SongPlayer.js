d13b011acc5d321b2f5d29dd27c6df59ab473b7b
jon manes <jon.manes@gmail.com>
2016-09-06 07:29:42 -0500

    Assignment 7

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,7 +1,20 @@
 (function() {
+    
+/**
+ * @function SongPlayer
+ * @desc Plays and pauses the selected song based on current state
+ * @param none
+ */
      function SongPlayer() {
+ /**
+ * @desc place holder for the methods to control the song actions; pause, play, etc 
+ * @type {Object}
+ */
           var SongPlayer = {};
-        
+ /**
+ * @desc currently selected song
+ * @type {Object}
+ */
           var currentSong = null;
          
  /**
@@ -29,23 +42,44 @@
 
             currentSong = song;
           };          
+          
+ /**
+ * @function playSong
+ * @desc Plays the selected song
+ * @param {Object} song
+ */
+     
+          var playSong = function(song) {
+              currentBuzzObject.play();
+              song.playing = true;
+          };
          
+ /**
+ * @function SongPlayer.play
+ * @desc Handles the logic for determining if the song is set, if not it sets and plays the song, if so it checkif song is paused and if so it plays the song. assumes if the song is set and not paused that it is playing.
+ * @param {Object} song
+ */
+     
           SongPlayer.play = function(song) {
               
             if (currentSong !== song) {
                  
                 setSong(song);
 
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
                 
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
+                    
                 }
             } 
           };
-         
+/**
+ * @function SongPlayer.pause
+ * @desc pauses the selected song
+ * @param {Object} song
+ */
           SongPlayer.pause = function(song) {
               currentBuzzObject.pause();
               song.playing = false;
